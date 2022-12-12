9460936b85e5ad53dab6aa16de7569b87cf39642
r1k1t0 <paezr2775@gmail.com>
2022-12-12 05:46:25 -0800

    Finished script for mountain filter
    Minor changes to html and css as well

--- scripts/mtnFilter.js
+++ scripts/mtnFilter.js
@@ -1,47 +1,68 @@
-window.onload = function LoadData() {
-    mountainCards()
-  }
+
+window.onload = function loadData() {
   
-  function mountainTemplate(mountain) {
-      // let mySunrise = getSunsetForMountain(`${mountain.coords.lat},${mountain.coords.lng}`);
-       
-       return `
-         <div class="mountain">
-         <img class="mountain-photo" src="images/${mountain.img}">
-         <h2 class="mountain-name">${mountain.name} <span class="species">(${mountain.elevation} feet)</span></h2>
-         <h4 class="mountain-desc">${mountain.desc} </h4>
-         <p><strong>Effort:</strong> ${mountain.effort}</p>
-         <strong>Coordinates:</strong> lat: ${mountain.coords.lat} lng: ${mountain.coords.lng}
+}
+
+
+//DropDown Menu
+function mountainDropDown(selectbox,text,value )
+{
+	var optn = document.createElement("option");
+	optn.text = text;
+	optn.value = value;
+	selectbox.options.add(optn);
+}
+
+function mountainDropDownList(){
+for (var i=0; i < mountainsArray.length;++i){
+    mountainDropDown(document.drop_list.mountainDropDown, mountainsArray[i].name);
+    }
+ 
+}
+
+function loadData() {
+    var down = document.getElementById('mountain');
+           for (let i = 0; i < mountainsArray.length; i++) {
+               var optn = mountainsArray[i];
+               var el = document.createElement("option");
+               el.textContent = optn;
+               el.value = optn;
+            //    down.appendChild(el);
+           }
+        //    down.innerHTML = "Elements Added";
+ }
+
+ //Template on how the mountain data would display
+function mountainTemplate(mountain) {
+    return `
+      <div class="mountain">
+      <img class="mountain-photo" src="images/${mountain.img}">
+      <h2 class="mountain-name">${mountain.name} <span class="species">(${mountain.elevation} feet)</span></h2>
+      <h4 class="mountain-desc">${mountain.desc} </h4>
+      <p>Elevation: ${mountain.elevation}</p>
+      </div>
       
-         </div>
-         `;
-     }
-  
-     ///mountain cards
-     function mountainCards() {
-     
-          document.getElementById("mountainsDiv").innerHTML = `
-            <h1 class="app-title"> ${mountainsArray.length} Mountains to climb</h1>
-            ${mountainsArray.map(mountainTemplate).join("")}
-            <p class="footer">These ${mountainsArray.length} mountants were added recently. Check back soon for updates.</p>
-          `;  
-     }
+    `};
 
-     function loadData() {
-        var down = document.getElementById('mountainDropDown');
-               for (let i = 0; i < mountainsArray.length; i++) {
-                   var optn = mountainsArray[i].name;
-                   var el = document.createElement("option");
-                   el.textContent = optn;
-                   el.value = optn;
-              down.appendChild(el);
-               }
-               ///down.innerHTML = "Elements Added";
-           }
-           loadData()
+
+
+function displayMountain() {
+ //saves drop down menu value
+   let mydropdown = document.getElementById('mountainDropDown');
+   let value = mydropdown.value;
+   let  selectedOption = mydropdown.options[mydropdown.selectedIndex].text;
+
+ //filters the array to match value
+   const filteredMountains = mountainsArray.filter(mountain => mountain.name === selectedOption);
+
+
+  document.getElementById("mountains").innerHTML = `
+    <h1 class="app-title"> Take a 'peak' at some of these mountains! </h1>
+    ${filteredMountains.map(mountainTemplate).join("")}
    
-           function dropDownSelect () {
-              mountainCards ()
-
-
-           }
+  `;  
+       
+}
+mountainDropDownList();
+loadData();
+displayMountain();
